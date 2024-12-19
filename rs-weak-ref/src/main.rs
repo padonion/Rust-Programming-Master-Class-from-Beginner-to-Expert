8ab85939550ead7c19815a28bf4171c2dbf7d078
Christophe K. <padonion@gmail.com>
2024-12-19 22:24:43 +0000

    add weak use example

---
+++ rs-weak-ref/src/main.rs
@@ -0,0 +1,29 @@
+use std::borrow::Borrow;
+use std::rc::{Rc, Weak};
+use std::cell::{RefCell, Ref};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf is {:?}", leaf);
+    println!("branch's leaf is {:?}", branch.children.borrow()[0]);
+}
