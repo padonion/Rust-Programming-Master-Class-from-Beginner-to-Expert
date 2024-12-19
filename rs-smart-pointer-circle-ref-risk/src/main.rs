e3bb894ea49ca74f4454894939a6093194c734a3
Christophe K. <padonion@gmail.com>
2024-12-19 22:07:04 +0000

    use Weak to avoid reference cycles

--- rs-smart-pointer-circle-ref-risk/src/main.rs
+++ rs-smart-pointer-circle-ref-risk/src/main.rs
@@ -1,9 +1,9 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct Node {
-    next: Option<Rc<RefCell<Node>>>,
+    next: Option<Weak<RefCell<Node>>>,
 }
 
 impl Drop for Node {
@@ -18,27 +18,27 @@ fn main() {
     println!("- After creating a:");
     println!("a strong = {}, weak = {}", Rc::strong_count(&a), Rc::weak_count(&a));
 
-    let b = Rc::new(RefCell::new(Node { next: Some(Rc::clone(&a)) }));
+    let b = Rc::new(RefCell::new(Node { next: Some(Rc::downgrade(&a)) }));
     println!("- After creating b:");
     println!("a strong = {}, weak = {}", Rc::strong_count(&a), Rc::weak_count(&a));
     println!("b strong = {}, weak = {}", Rc::strong_count(&b), Rc::weak_count(&b));
 
-    let c = Rc::new(RefCell::new(Node { next: Some(Rc::clone(&b)) }));
+    let c = Rc::new(RefCell::new(Node { next: Some(Rc::downgrade(&b)) }));
     println!("- After creating c:");
     println!("a strong = {}, weak = {}", Rc::strong_count(&a), Rc::weak_count(&a));
     println!("b strong = {}, weak = {}", Rc::strong_count(&b), Rc::weak_count(&b));
     println!("c strong = {}, weak = {}", Rc::strong_count(&c), Rc::weak_count(&c));
 
     //  following line to create a cycle
-    (*a).borrow_mut().next = Some(Rc::clone(&c));
+    (*a).borrow_mut().next = Some(Rc::downgrade(&c));
     println!("- After creating cycle:");
     println!("a strong = {}, weak = {}", Rc::strong_count(&a), Rc::weak_count(&a));
     println!("b strong = {}, weak = {}", Rc::strong_count(&b), Rc::weak_count(&b));
     println!("c strong = {}, weak = {}", Rc::strong_count(&c), Rc::weak_count(&c));
 
     //  following line will create a cycle
-    //println!("a is {:?}", a);
+    println!("a is {:?}", a);
 
-    // break the cycle
-    (*a).borrow_mut().next = None;
+    // break the cycle (not needed anymore)
+    //(*a).borrow_mut().next = None;
 }
